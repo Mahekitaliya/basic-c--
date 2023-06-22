32c6d7f8b33bdfcf3a4d2611e6a098e1423b5b42
mahek <mahekitaliya2005@gmail.cpm>
2023-06-21 20:34:55 -0700

    basic-m2

---
+++ m6.cpp
@@ -0,0 +1,19 @@
+//m6.cpp
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"before if";
+    if(1)
+    {
+        cout<<"\n \tturepart";
+    }
+        
+    else
+    {
+        cout<<"\n\t flasepart";
+    
+    }
+    cout<<"\nafter if";
+    
+}
---
+++ m7.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cout<<"Enter n:";
+    cin>>n;
+    cout<<"Enter m:";
+    cin>>m;
+    if(n<m)
+    {
+        cout<<"\n smallest value = "<<n;
+    }
+    else
+    {
+        cout<<"\n smallest value ="<<m;
+    
+    }
+}
